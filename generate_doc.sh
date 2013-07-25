15687276cb8a7e3eb931202917a04e006be214e3
Vladimir Vitvitskiy <vladimir.vitvitskiy@hp.com>
2013-07-25 15:31:40 +0300

    fix resource reading using classloader

--- generate_doc.sh
+++ generate_doc.sh
@@ -1,3 +1,3 @@
 CM_PATH=/Users/signalpillar/proj/git-content-main/discovery/packages/ucmdb9-ddm-layout/src/main/resources/runtime/probeManager/discoveryConfigFiles/__CmdbClassModel.bin
 PP_PATH=/Users/signalpillar/proj/git-content-main/discovery/packages
-java -jar ddm-doc.0.0.1.jar -cm $CM_PATH -pp $PP_PATH $@
+java -jar target/ddm-doc-0.0.2-standalone.jar -cm $CM_PATH -pp $PP_PATH $@
--- src/ddm_doc/md_generator.clj
+++ src/ddm_doc/md_generator.clj
@@ -1,27 +1,30 @@
 (ns ddm-doc.md-generator
   (:use fleet))
 
+(defn find-template [name]
+  (.findResource (.getContextClassLoader (Thread/currentThread)) name))
+
 (extend-protocol fleet.runtime/Screenable
   nil
   (screen [s escaping-fn] "None"))
 
 (defn generate-adapter [adapter]
-  (let [content (slurp "src/resources/adapter.md")
+  (let [content (slurp (find-template "resources/adapter.md"))
         tpl (fleet [pattern] content)]
     (tpl adapter)))
 
 (defn generate-job [job]
-  (let [content (slurp "src/resources/job.md")
+  (let [content (slurp (find-template "resources/job.md"))
         tpl (fleet [job] content)]
     (tpl job)))
 
 (defn generate-reference [pattern-to-jobs-pairs]
-  (let [content (slurp "src/resources/reference.md")
+  (let [content (slurp (find-template "resources/reference.md"))
         tpl (fleet [reference] content)]
     (tpl {:patterns (map first pattern-to-jobs-pairs)
           :jobs (mapcat second pattern-to-jobs-pairs)})))
 
 (defn generate-general [reference]
-  (let [content (slurp "src/resources/general.md")
+  (let [content (slurp (find-template "resources/general.md"))
         tpl (fleet [reference] content)]
     (tpl {:content (str reference)})))
