9457cd0cdda2c6e23b3d8f56f83747edcb8a8a15
Vladimir Vitvitskiy <vladimir.vitvitskiy@hp.com>
2013-07-25 11:45:34 +0300

    implement command line tool

--- src/ddm_doc/core.clj
+++ src/ddm_doc/core.clj
@@ -6,7 +6,6 @@
             [ddm-doc.patterns :as ddmpatterns]
             [ddm-doc.md-generator :as md-gen]))
 
-
 (def ddm-root-path "/Users/signalpillar/proj/git-content-main/discovery/packages")
 (def class-model-path "/Users/signalpillar/proj/git-content-main/discovery/packages/ucmdb9-ddm-layout/src/main/resources/runtime/probeManager/discoveryConfigFiles/__CmdbClassModel.bin")
 
@@ -25,7 +24,10 @@
       (read r))))
 
 
-(defn read-data [ & {:keys [serialized? file-path]
+(defn read-data
+  "Read information about available adapters, jobs and class-model entries
+After each call save serialized version of gathered data"
+  [ & {:keys [serialized? file-path]
                      :or {serialized? false
                           file-path "serialized.txt"}}]
   (if (and serialized? (.exists (file file-path)))
@@ -42,30 +44,52 @@
       (serialize file-path data)
       data)))
 
-(defn prepare-adapter [adapter class-by-name]
-  (let [classes (:discovered-classes adapter)]
-    ;(assoc adapter :discovered-classes (map (comp :display-lable class-by-name) classes))
-    classes
-    ))
+(defn find-entry-point-module
+  "Find python entry point module in declared used-scripts section in adapter.
+In case if list is indexed - script with maximum index is taken, otherwise
+take the latest script in the list
+@types: list -> dict
+"
+  [scripts]
+  (if (-> scripts first :index empty?)
+    (last scripts)
+    (apply max-key #(Integer/parseInt (:index %)) scripts)))
 
-(defn build-adapters&jobs-doc [adapter-ids]
+(defn prepare-adapter
+  "Prepare adapter for the documenting
+- replace reported CITs by display labels
+- replace used scripts by only one entry point module"
+  [adapter class-by-name]
+  (let [cits (:discovered-classes adapter)
+        scripts (:used-scripts adapter)
+        display-names (map (comp :display-name class-by-name) cits)]
+    (assoc adapter
+      :discovered-classes display-names
+      :used-scripts [(find-entry-point-module scripts)])))
+
+(defn build-adapters&jobs-doc
+  "Build data ready to be included in the documentation - list of pairs
+adapter to jobs declared on top of it"
+  [adapter-ids]
   (let [data (read-data :serialized? false)
+        class-by-name (:class-by-name data)
         pattern-by-id (:pattern-by-id data)
         job-by-id (:job-by-id data)]
     (for [id adapter-ids]
-      (let [adapter (pattern-by-id id)
+      (let [adapter (prepare-adapter (pattern-by-id id) class-by-name)
             jobs (filter #(= id (:pattern-id %)) (vals job-by-id))
             adapter-doc (md-gen/generate-adapter adapter)
             jobs-doc (map md-gen/generate-job jobs)]
         [adapter-doc jobs-doc]))))
 
-(defn build-doc [& adapter-ids]
+(defn build-doc
+  "Build general documentation that includes
+- concepts and introductional section
+- reference part
+  - adapters
+  - jobs"
+  [adapter-ids]
   (let [adapter-to-jobs-pairs (build-adapters&jobs-doc adapter-ids)
         reference-doc (md-gen/generate-reference adapter-to-jobs-pairs)
         general-doc (md-gen/generate-general reference-doc)]
     general-doc))
-
-(with-open [w (writer "document.md")]
-  (binding [*out* w]
-    (let [doc (build-doc "DNS_Zone")]
-      (println (.toString doc)))))
---
+++ src/ddm_doc/generatoren.clj
@@ -0,0 +1,41 @@
+(ns ^{:author "Vladimir Vitvitskiy"}
+  ddm-doc.generatoren
+  (:require [clojure.tools.cli :as cli]
+            [ddm-doc.core])
+  (:gen-class))
+
+(def HEADER "Tool to generate documentation for adapter and related jobs
+Example,
+java -jar ddm-doc.jar -cm <path-to-class-model> -pp <path-to-packages> <adapter-id1> <adapter-id2> > doc.md
+")
+
+(defn- add-header [banner]
+  (format "%s
+%s
+" HEADER banner))
+
+(defn- error [banner & msgs]
+  (do
+    (println (apply str msgs))
+    (println (add-header banner))
+    (System/exit 1)))
+
+(defn- help [banner]
+  (do
+    (println (add-header banner))
+    (System/exit 0)))
+
+(defn -main [ & arguments]
+  (let [[options args banner] (cli/cli arguments
+                                       ["-h" "--help" "Print this help message" :flag true]
+                                       ["-j" "--jobs-only" "Generate documentation for the jobs only belonging to the adapters" :flag true]
+                                       ["-cm" "--classmodel-path" "Path to the .bin file of the class model"]
+                                       ["-pp" "--packages-path" "Path to the root folder where package(s) are stored"]
+                                       )
+        adapter-ids args]
+    (cond
+     (:help options) (help)
+     (not (seq adapter-ids)) (error banner "Adapter IDs are not specified")
+     (not (:packages-path options)) (error banner "Path to the packages is not specified")
+     (not (:classmodel-path options)) (error banner "Path to the class model file is not specified"))
+    (println  (str (ddm-doc.core/build-doc adapter-ids)))))
