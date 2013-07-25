b1a224e160db942126e422b20dcc146f59b53803
Vladimir Vitvitskiy <vladimir.vitvitskiy@hp.com>
2013-07-25 14:56:52 +0300

    new version released

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject ddm-doc "0.0.1-SNAPSHOT"
-  :description "Cool new project to do things and stuff"
+(defproject ddm-doc "0.0.2"
+  :description "Tool to generate ddm documentation in markdown"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [fleet "0.10.1"]
                  [org.clojure/tools.cli "0.2.2"]]
