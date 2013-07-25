6f828abfd5680561402c8cab74b512e930b73b1f
Vladimir Vitvitskiy <vladimir.vitvitskiy@hp.com>
2013-07-25 14:35:18 +0300

    update built version

---
+++ generate_doc.sh
@@ -0,0 +1,3 @@
+CM_PATH=/Users/signalpillar/proj/git-content-main/discovery/packages/ucmdb9-ddm-layout/src/main/resources/runtime/probeManager/discoveryConfigFiles/__CmdbClassModel.bin
+PP_PATH=/Users/signalpillar/proj/git-content-main/discovery/packages
+java -jar ddm-doc.0.0.1.jar -cm $CM_PATH -pp $PP_PATH $@
