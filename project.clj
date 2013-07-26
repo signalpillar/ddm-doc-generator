b453e911ccd5e5c6c010c0ed2f628132f1d13e87
Vladimir Vitvitskiy <vladimir.vitvitskiy@hp.com>
2013-07-26 18:43:27 +0300

    depend on panda-cheese to deal with TQLs

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Tool to generate ddm documentation in markdown"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [fleet "0.10.1"]
-                 [org.clojure/tools.cli "0.2.2"]]
+                 [org.clojure/tools.cli "0.2.2"]
+                 [panda-cheese "0.1.3"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
   :main ddm-doc.generatoren)
