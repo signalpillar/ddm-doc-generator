8a04807293f6895ec75e95f35b6cf801597a647a
Vladimir Vitvitskiy <vladimir.vitvitskiy@hp.com>
2013-08-20 13:36:17 +0300

    support the latest verion of lein for compilation

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
                  [org.clojure/tools.cli "0.2.2"]
                  [panda-cheese "0.1.3"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
+  :aot [ddm-doc.generatoren]
   :main ddm-doc.generatoren)
