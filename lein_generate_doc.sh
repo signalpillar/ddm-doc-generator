0197d712a67c0d75099b0e9cd0c3cc5b48e99189
Vladimir Vitvitskiy <vladimir.vitvitskiy@hp.com>
2013-08-28 23:08:11 +0300

    update helper scripts

--- lein_generate_doc.sh
+++ lein_generate_doc.sh
@@ -1,3 +1,3 @@
 CM_PATH=~/proj/git-content-main/discovery/packages/ucmdb9-ddm-layout/src/main/resources/runtime/probeManager/discoveryConfigFiles/__CmdbClassModel.bin
 PP_PATH=~/proj/git-content-main/discovery/packages
-java -jar target/ddm-doc-0.0.2-standalone.jar -cm $CM_PATH -pp $PP_PATH $@
+lein run -- $CM_PATH $PP_PATH $@
